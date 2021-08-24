ae58fe4bc1cf53dd164ff3db2e12fcf99a127fa9
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-24 14:45:30 -0500

    DONE: PlusMinus

---
+++ src/finsihed/PlusMinus.java
@@ -0,0 +1,52 @@
+package finsihed;
+
+import javax.xml.transform.Result;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class PlusMinus {
+
+    public static void plusMinus(List<Integer> arr) {
+        float positives = 0;
+        float negatives = 0;
+        float zeros = 0;
+        //run through code and get total positives, negatives & zero
+        for (int number : arr) {
+            if (number > 0) {
+                positives++;
+            } else if (number < 0) {
+                negatives++;
+            } else if (number == 0) {
+                zeros++;
+            }
+        }
+        //print positives / size
+        System.out.println(positives / arr.size());
+        //print negatives / size
+        System.out.println(negatives / arr.size());
+        //print zeros / size
+        System.out.println(zeros / arr.size());
+    }
+
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(-4);
+        list.add(3);
+        list.add(-9);
+        list.add(0);
+        list.add(4);
+        list.add(1);
+        System.out.println("Expected: .5, .333, .1667");
+        plusMinus(list);
+    }
+}
+
