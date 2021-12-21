c29a9f4e35225a484751d6b8ddbfb3a5e5df309f
calaisgalbraith <calaisgalbraith@gmail.com>
2021-12-20 20:13:13 -0700

    FINALLY FINISHED

--- src/finished/CamelCase.java
+++ src/finished/CamelCase.java
@@ -2,6 +2,7 @@ package finished;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class CamelCase {
@@ -14,9 +15,23 @@ public class CamelCase {
         //ArrayList to hold inputs
         List<String> inputs = new ArrayList<>();
 
-        while(inputs.size() < 4){
-            String scannerInput = sc.nextLine();
-            inputs.add(scannerInput);
+        //placeholder for scanner input
+        String scannerInput = "";
+
+        boolean continueReading = true;
+
+        //keep reading scanner input until no more lines
+        while(continueReading){
+            try{
+                scannerInput = sc.nextLine();
+                if(scannerInput.equals("C;C;mirror")){
+                    continueReading = false;
+                }
+                inputs.add(scannerInput);
+            }
+            catch (NoSuchElementException e){
+                continueReading = false;
+            }
         }
 
         //loop through each string in array
@@ -30,7 +45,7 @@ public class CamelCase {
                 //checks if method
                 if (input.charAt(2) == 'M') {
                     input = input.replaceFirst("S;M;", ""); //remove first four char
-                    input = input.replaceFirst("()", ""); // remove the () at the end
+                    input = input.substring(0, input.length() - 2); //remove ()
                 }
 
                 //checks if class
@@ -61,40 +76,36 @@ public class CamelCase {
                 //checks if method
                 if (input.charAt(2) == 'M') {
                     input = input.replaceFirst("C;M;", ""); //remove first four char
-                    input = input.replaceAll(" ", ""); //remove all whitespace
-                    toReturn = input + "()"; //add closing brackets
+                    input = input + "()"; //add closing brackets
                 }
 
+                //checks if class
+                else if (input.charAt(2) == 'C') {
+                    input = input.replaceFirst("C;C;", ""); //remove first four char
+                    input = Character.toUpperCase(input.charAt(0)) + input.substring(1); //make first letter upperCase
+                }
+
+                //else is variable
                 else {
-                    //checks if class
-                    if (input.charAt(2) == 'C') {
-                        input = input.replaceFirst("C;C;", ""); //remove first four char
-                        input = Character.toUpperCase(input.charAt(0)) + input.substring(1); //make first letter upperCase
+                    input = input.replaceFirst("C;V;", ""); //remove first four char
+                }
+
+                boolean makeNextCapital = false;
+
+                 //loop through each letter in string
+                for (char letter : input.toCharArray()) {
+                    //if char is a space, remove and make next one uppercase
+                    if (letter == ' ') {
+                        makeNextCapital = true;
                     }
-
-                    //else is variable
-                    else {
-                        input = input.replaceFirst("C;V;", ""); //remove first four char
+                    //check if make next letter capital
+                    else if (makeNextCapital){
+                        toReturn = toReturn + Character.toUpperCase(letter);
+                        makeNextCapital = false; //reset makeNextCapital
                     }
-
-                    boolean makeNextCapital = false;
-
-                    //loop through each letter in string
-                    for (char letter : input.toCharArray()) {
-
-                        //if char is a space, remove and make next one uppercase
-                        if (letter == ' ') {
-                            makeNextCapital = true;
-                        }
-                        //check if make next letter capital
-                        else if (makeNextCapital){
-                            toReturn = toReturn + Character.toUpperCase(letter);
-                            makeNextCapital = false; //reset makeNextCapital
-                        }
-                        //else just add next letter as is
-                        else{
-                            toReturn = toReturn + letter;
-                        }
+                    //else just add next letter as is
+                    else{
+                        toReturn = toReturn + letter;
                     }
                 }
             }
