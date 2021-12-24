3e8e2a5ba53c867041e303821e9ceb8f95a20071
calaisgalbraith <calaisgalbraith@gmail.com>
2021-12-24 10:58:45 -0700

    works within timeframe

--- src/MinimumAbsoluteDifference.java
+++ src/MinimumAbsoluteDifference.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MinimumAbsoluteDifference {
@@ -6,24 +9,44 @@ public class MinimumAbsoluteDifference {
 
     public static int minimumAbsoluteDifference(List<Integer> arr) {
 
+
+        //refactored for time constraints
         int minimum = Integer.MAX_VALUE; //placeholder for minimum
         int absoluteDifference = 0; //placeholder for absolute difference
 
-        //loop through each int in list except last one (bc no need)
-        for(int i = 0; i < arr.size() - 1 ; i++){
+        //sort array
+        Collections.sort(arr);
 
-            //loop through each remaining in in array
-            for(int j = i + 1; j < arr.size(); j++){
-                absoluteDifference = Math.abs(arr.get(i) - arr.get(j));
+        //loop through each int in list except last one bc no need
+        for(int i = 0; i < arr.size() - 1; i++){
+            absoluteDifference = Math.abs(arr.get(i) - arr.get(i+1));
 
-                //if absolute difference is less than minimum
-                if(absoluteDifference < minimum){
-                    minimum = absoluteDifference;
-                }
+            if(absoluteDifference < minimum){
+                minimum = absoluteDifference;
             }
-        }
 
+        }
         return minimum;
+
+
+//        int minimum = Integer.MAX_VALUE; //placeholder for minimum
+//        int absoluteDifference = 0; //placeholder for absolute difference
+//
+//        //loop through each int in list except last one (bc no need)
+//        for(int i = 0; i < arr.size() - 1 ; i++){
+//
+//            //loop through each remaining in in array
+//            for(int j = i + 1; j < arr.size(); j++){
+//                absoluteDifference = Math.abs(arr.get(i) - arr.get(j));
+//
+//                //if absolute difference is less than minimum
+//                if(absoluteDifference < minimum){
+//                    minimum = absoluteDifference;
+//                }
+//            }
+//        }
+//
+//        return minimum;
     }
 
     public static void main(String[] args) {
