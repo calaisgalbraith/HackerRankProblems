998a2c5053c737bb6213283884566f297fe021f8
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-07 10:33:20 -0600

    finished flipping bits (woo)

--- src/finished/FlippingBits.java
+++ src/finished/FlippingBits.java
@@ -1,4 +1,8 @@
+package finished;
+
 public class FlippingBits {
+
+    //given a list of 32 bit unsigned integers, flip all bits (0 -> 1 && 1 -> 0) and return result as an unsigned integer
 
     public static long flippingBits(long n) {
 
@@ -6,6 +10,12 @@ public class FlippingBits {
 
         //convert long to string binary
         String binary = Long.toBinaryString(n);
+
+        //working with 32 bit integer, so if length of binary is not 32, add leading 0s until works
+        //check length of int and add leading 0s until length is 32
+        for(int i = binary.length(); i < 32; i++){
+            binary = "0" + binary;
+        }
 
         //go through each number & flip 0 to 1 (and 1 to 0)
         for(char number : binary.toCharArray()){
@@ -22,7 +32,6 @@ public class FlippingBits {
     }
 
     public static void main(String[] args) {
-
-        System.out.println();
+        System.out.println(flippingBits(1));
     }
 }
