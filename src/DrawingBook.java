d8788cfed1b51d554b211c014b50f08e556079a0
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-18 11:06:15 -0600

    finished drawing-book

--- src/DrawingBook.java
+++ src/DrawingBook.java
@@ -5,45 +5,17 @@ public class DrawingBook {
 
 
     public static int pageCount(int n, int p) {
-        int pages = 0;
 
-        int holder = (n-p) / 2;
+        int pageCountFront = n / 2;
+        int targetPageCountFront = p / 2;
+        int targetPageCountBack = pageCountFront - targetPageCountFront;
 
-        if(holder % 2 == 0){
-            return holder;
-        }
-        else{
-            return (int) (holder - .5);
-        }
-
-
-
-//        //if desired page is 1 or the end of the book, no need to turn any pages
-//        //if n is odd, and p is equal to n -1, also no need to turn any pages
-//            //ex 4 & 5 are on the same page so if p = 4 & n=5, then no pages needed
-//        if(p == 1 || p == n || ((n % 2 != 0 && p == n - 1))){
-//            return 0;
-//        }
-//
-//        //check front of book
-//        //while i is less than # page in book, flip page (increase pages) & increase i by 2,
-//        for(int i = 1; i < p; i += 2){
-//            pages++;
-//        }
-//
-//        //check back of book
-//        int backPages = 0;
-//        for(int i = n; i >= p ; i -= 2){
-//            backPages++;
-//        }
-//
-//        System.out.println("front " + pages);
-//        System.out.println("back " + backPages);
+        return Math.min(targetPageCountFront, targetPageCountBack);
 
     }
 
 
     public static void main(String[] args) {
-        System.out.println(pageCount(6, 5));
+        System.out.println(pageCount(7, 4));
     }
 }
